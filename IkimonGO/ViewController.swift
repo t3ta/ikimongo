19997b9239977643517f4878ba4dee286e965236
Takahito Mita <astm1125.tsukuba@gmail.com>
2019-02-22 13:26:58 +0900

    made LoginViewController files

--- IkimonGO/ViewController.swift
+++ IkimonGO/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  IkimonGO
 //
 //  Created by Takahito Mita on 2019/02/22.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
