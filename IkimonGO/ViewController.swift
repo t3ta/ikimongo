4cf24c3df59c2a6e29defb5569d6d7bebe6040d3
Takahito Mita <astm1125.tsukuba@gmail.com>
2019-02-22 13:24:57 +0900

    Initial Commit

---
+++ IkimonGO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IkimonGO
+//
+//  Created by Takahito Mita on 2019/02/22.
+//  Copyright © 2019 Takahito Mita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
